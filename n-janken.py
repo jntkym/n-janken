4649205b2d04cafcc223787ec76a398f674309f1
jntkym <ru6r1ckcu6e@gmail.com>
2016-01-07 21:58:56 +0900

    add comments

--- n-janken.py
+++ n-janken.py
@@ -9,12 +9,13 @@ import random
 def main(args):
   candidates = args
   for i in range(0, len(candidates)):
+    # TODO: implement "aiko"
     print candidates[i].decode('utf-8'), (random.choice(['✊', '✌️', '✋'])).decode('utf-8')
-  # TODO: implement "aiko"
 
 if __name__ == '__main__':
   sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
 
+  # TODO: add arg for simple function
   parser = argparse.ArgumentParser()
   parser.add_argument("-name", help="list of name", nargs='+')
   args = parser.parse_args()
